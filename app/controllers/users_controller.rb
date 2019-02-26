ce2ace1d780cd969e360057a1ddfe2ed95d7e23d
Kevin Huang <thekevinhuang@gmail.com>
2019-02-26 00:36:52 -0500

    added react frontend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
 
     def create
-        
+        @user = User.new(user_params)
+
+        if @user.save
+            render json: @user
+        else
+
+        end
+
     end
 
     def show
+        @user = User.find_by(id: params[:id])
 
+        render json: @user
     end
 
     def index
@@ -15,6 +24,10 @@ class UsersController < ApplicationController
     end
 
     private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
 
 
 
